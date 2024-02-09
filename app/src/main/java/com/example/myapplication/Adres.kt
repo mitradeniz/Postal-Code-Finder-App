c3a03190a6b0f68044f5cafb6c7ae8feb87122bf
mitradeniz <65504062+mitradeniz@users.noreply.github.com>
2024-02-09 17:15:59 +0300

    Delete app/src/main/java/com/example/myapplication/Adres.kt

--- app/src/main/java/com/example/myapplication/Adres.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-data class Adres(
-    val il: String,
-    val ilçe: String,
-    val semt_bucak_belde: String,
-    val Mahalle: String,
-    val PK: String
-)
