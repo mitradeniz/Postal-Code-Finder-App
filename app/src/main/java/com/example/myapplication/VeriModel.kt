0be72924926a0dc6a079edfb4d6a9d0a407cab27
mitradeniz <65504062+mitradeniz@users.noreply.github.com>
2024-02-09 17:16:11 +0300

    Delete app/src/main/java/com/example/myapplication/VeriModel.kt

--- app/src/main/java/com/example/myapplication/VeriModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-data class VeriModel(
-    val il: String,
-    val ilçe: String,
-    val semt_bucak_belde: String,
-    val Mahalle: String,
-    val PK: String
-)
